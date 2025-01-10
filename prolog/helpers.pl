33c5d22c06f77f76b81035c15a9d52636fd33603
Damian Spigut <damino@Damians-MacBook-Pro.local>
2025-01-10 18:24:03 +0000

    Define min/max and row/column manipulation helper functions

--- prolog/helpers.pl
+++ prolog/helpers.pl
@@ -1,9 +1,42 @@
 % Solution script for part a) of the coursework.
 
+all_rows(Rs):-
+    setof(Rows, X^Y^input_colour(Rows, X, Y), Rs),
+    !.
+
+all_cols(Cs):-
+    setof(Col, input_colour(_, Col, _), Cs),
+    !.
+
 % This is a dummy predicate. You should fill in as instructed in the coursework spec
 row(R):-
-    R is 0.
+    all_rows(Rs),
+    member(R, Rs).
 
 % This is a dummy predicate. You should fill in as instructed in the coursework spec
 col(C):-
-    C is 0.
+    all_cols(Cs),
+    member(C, Cs).
+
+nrows(NR):-
+    all_rows(Rs),
+    length(Rs, NR).
+
+ncols(NC):-
+    all_cols(Cs),
+    length(Cs, NC).
+
+min([], _):-
+    false.
+
+min(L, Min):-
+    sort(L, [Min | _]).
+
+max([X | XS], Max) :-
+    max_acc([X | XS], X, Max).
+
+max_acc([], CurrentMax, CurrentMax).
+
+max_acc([X | XS], CurrentMax, Max) :-
+    NewMax is max(X, CurrentMax),
+    max_acc(XS, NewMax, Max).
