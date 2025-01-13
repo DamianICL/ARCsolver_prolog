6c89629a05186141118e4e29fec6a21aa4df67c0
Damian Spigut <damino@Damians-MacBook-Pro.local>
2025-01-13 16:45:57 +0000

    Optimize finding the number of grey adjacent tiles

--- prolog/solution_2.pl
+++ prolog/solution_2.pl
@@ -25,14 +25,13 @@ output_colour(Row,Column,Colour):-
                       (X1 is R - 1, Y1 is C, in_square((X1, Y1)))), Blues
                       ),
     Blues = [(BX, BY)],
-    DX is R - BX,
-    DY is C - BY,
+    DX is R - BX, DY is C - BY,
     Direction = (DX, DY),
-    in_line((R, C), Direction, (Row, Column)),
+    draw_line((R, C), Direction, (Row, Column)),
     Colour = teal.
 
 unzip3([], [], [], []).
-unzip3([(X, Y, Z) | Rest], [X | Xs], [Y | Ys], [Z | Zs]) :-
+unzip3([(X, Y, Z) | Rest], [X | Xs], [Y | Ys], [Z | Zs]):-
     unzip3(Rest, Xs, Ys, Zs).
 
 bounds(MinX, MaxX, MinY, MaxY) :-
@@ -54,18 +53,23 @@ in_square((X, Y)) :-
     Y < MaxY, Y > MinY.
 
 num_grey_adjacent_tiles((X, Y), Grey):-
-    findall((X1, Y1), ((X1 is X, Y1 is Y - 1, input_colour(X1, Y1, grey));
-                      (X1 is X, Y1 is Y + 1, input_colour(X1, Y1, grey));
-                      (X1 is X + 1, Y1 is Y, input_colour(X1, Y1, grey));
-                      (X1 is X - 1, Y1 is Y, input_colour(X1, Y1, grey))), Greys
-                      ),
-    length(Greys, Grey).
+    X1 is X + 1, X2 is X - 1, Y1 is Y + 1, Y2 is Y - 1,
+    is_grey(X, Y2, G1),
+    is_grey(X, Y1, G2),
+    is_grey(X1, Y, G3),
+    is_grey(X2, Y, G4),
+    Grey is G1 + G2 + G3 + G4.
 
-in_line((X, Y), _, (X, Y)).
+is_grey(X, Y, 1) :-
+    input_colour(X, Y, grey),
+    !.
+is_grey(_, _, 0).
 
-in_line((X, Y), (D, D1), Square):-
+draw_line((X, Y), _, (X, Y)).
+
+draw_line((X, Y), (D, D1), Square):-
     row(X),
     col(Y),
     NewX is X + D,
     NewY is Y + D1,
-    in_line((NewX, NewY), (D, D1), Square).
+    draw_line((NewX, NewY), (D, D1), Square).
