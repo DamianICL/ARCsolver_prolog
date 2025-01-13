52ec3c3bbde46713749ae74a036195efd624a59e
Damian Spigut <damino@Damians-MacBook-Pro.local>
2025-01-13 15:58:44 +0000

    Refactor code into relevant directory

--- prolog/solution_1.pl
+++ prolog/solution_1.pl
@@ -16,3 +16,49 @@ output_colour(Row,Column,Colour):-
      which_coloured(Starts),
      distance_apart(Starts, Step),
      row_colour(Starts, Row, Step, Colour).
+
+edge_row((X, _, _)) :-
+    all_rows(Rows),
+    max(Rows, Max),
+    (X = 0 ; X = Max).
+
+is_col([(X, Y, _), (X1, Y1, _)]):-
+    Y \= Y1,
+    edge_row((X, Y, black)),
+    edge_row((X1, Y1, black)).
+
+distance_apart([(X, Y, _), (X1, Y1, _)], Distance):-
+    (edge_row((X, Y, black)), edge_row((X1, Y1, black))),
+    !,
+    Distance is abs(Y - Y1).
+
+distance_apart([(X, _, _), (X1, _, _)], Distance):-
+    Distance is abs(X-X1).
+
+row_colour([(X, _, _), (X1, _, _)], X2, _, Colour) :-
+    X2 < min(X, X1),
+    !,
+    Colour = black.
+row_colour([(X, _, C), _], X2, Step, Colour) :-
+    0 is (X2 - X) mod (Step * 2),
+    !,
+    Colour = C.
+row_colour([_, (X1, _, C1)], X2, Step, Colour) :-
+    0 is (X2 - X1) mod (Step * 2),
+    !,
+    Colour = C1.
+row_colour(_, _, _, black).
+
+line_colour([(X, _), (X1, _)], X2, _, Colour) :-
+    X2 < min(X, X1),
+    !,
+    Colour = black.
+line_colour([(X, C), _], X2, Step, Colour) :-
+    0 is (X2 - X) mod (Step * 2),
+    !,
+    Colour = C.
+line_colour([_, (X1, C1)], X2, Step, Colour) :-
+    0 is (X2 - X1) mod (Step * 2),
+    !,
+    Colour = C1.
+line_colour(_, _, _, black).
--- prolog/solution_2.pl
+++ prolog/solution_2.pl
@@ -1,5 +1,7 @@
 % Solution script for the task 'pour' (data/training/d4f3cd78.json)
 
+:- dynamic stored_bounds/4.
+
 output_colour(Row,Column,Colour):-
     input_colour(Row, Column, grey),
     !,
@@ -28,3 +30,42 @@ output_colour(Row,Column,Colour):-
     Direction = (DX, DY),
     in_line((R, C), Direction, (Row, Column)),
     Colour = teal.
+
+unzip3([], [], [], []).
+unzip3([(X, Y, Z) | Rest], [X | Xs], [Y | Ys], [Z | Zs]) :-
+    unzip3(Rest, Xs, Ys, Zs).
+
+bounds(MinX, MaxX, MinY, MaxY) :-
+    stored_bounds(MinX, MaxX, MinY, MaxY), 
+    !.
+
+bounds(MinX, MaxX, MinY, MaxY) :-
+    which_coloured(Colours),
+    unzip3(Colours, Xs, Ys, _),
+    min(Xs, MinX),
+    max(Xs, MaxX),
+    min(Ys, MinY),
+    max(Ys, MaxY),
+    asserta(stored_bounds(MinX, MaxX, MinY, MaxY)).
+
+in_square((X, Y)) :-
+    bounds(MinX, MaxX, MinY, MaxY),
+    X < MaxX, X > MinX,
+    Y < MaxY, Y > MinY.
+
+num_grey_adjacent_tiles((X, Y), Grey):-
+    findall((X1, Y1), ((X1 is X, Y1 is Y - 1, input_colour(X1, Y1, grey));
+                      (X1 is X, Y1 is Y + 1, input_colour(X1, Y1, grey));
+                      (X1 is X + 1, Y1 is Y, input_colour(X1, Y1, grey));
+                      (X1 is X - 1, Y1 is Y, input_colour(X1, Y1, grey))), Greys
+                      ),
+    length(Greys, Grey).
+
+in_line((X, Y), _, (X, Y)).
+
+in_line((X, Y), (D, D1), Square):-
+    row(X),
+    col(Y),
+    NewX is X + D,
+    NewY is Y + D1,
+    in_line((NewX, NewY), (D, D1), Square).
